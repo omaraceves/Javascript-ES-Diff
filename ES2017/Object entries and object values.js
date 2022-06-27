a4792a1eb9eb120d0eaa803fa4344182fff0e098
Omar.Aceves <lcc.omar.aceves@gmail.com>
2022-06-26 23:05:18 -0700

    Object entries and object values

--- ES2017/Object entries and object values.js
+++ ES2017/Object entries and object values.js
@@ -2,4 +2,25 @@
 
 
 //Object.entries()
-//This is cheating, I was not able to code today
+//Returns the key values within an object as an array
+
+const cayman718 = {brand: 'Porsche', name: 'Cayman 718', cylinders: '4'};
+console.log(cayman718);  //{ brand: 'Porsche', name: 'Cayman 718', cylinders: '4' }
+
+console.log(Object.entries(cayman718));
+
+// [
+//     [ 'brand', 'Porsche' ],
+//     [ 'name', 'Cayman 718' ],
+//     [ 'cylinders', '4' ]
+// ]
+
+//Object.Values()
+//It works much like Object.entries(), but, as its name suggests, 
+//it only returns the values of the own enumerable string-keyed properties:
+
+const cayman718 = {brand: 'Porsche', name: 'Cayman 718', cylinders: '4'};
+console.log(cayman718);  //{ brand: 'Porsche', name: 'Cayman 718', cylinders: '4' }
+
+console.log(Object.values(cayman718)); //[ 'Porsche', 'Cayman 718', '4' ]
+
